f63446cf814954db2a1b5037e99555b805857bf8
Sparsh Choudhary <sparsht123t@gmail.com>
2021-09-20 15:41:13 +0530

    VeloityX,Home_detail_page

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,34 +1,39 @@
 class CatalogModel {
-  static List<Item> ?items;
+  static List<Item>? items;
 }
 
 class Item {
-  final int id;
-  final String name;
-  final String desc;
-  final num price;
-  final String color;
-  final String image;
+  int id;
+  String name;
+  String desc;
+  num price;
+  String color;
+  String image;
 
-  Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
+  Item({
+    required this.id,
+    required this.name,
+    required this.desc,
+    required this.price,
+    required this.color,
+    required this.image,
+  });
 
-
-
-    factory Item.fromJson(Map<String, dynamic> json) => Item(
+  factory Item.fromJson(Map<String, dynamic> json) => Item(
         id: json["id"],
         name: json["name"],
         desc: json["desc"],
         price: json["price"],
         color: json["color"],
         image: json["image"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "id": id,
         "name": name,
         "desc": desc,
         "price": price,
         "color": color,
         "image": image,
-    };
+      };
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,11 @@
 import 'dart:convert';
 
 import 'package:e_commerce/models/catalog.dart';
-import 'package:e_commerce/widgets/drawer.dart';
-import 'package:e_commerce/widgets/item_widget.dart';
+import 'package:e_commerce/widgets/home_widget/catalog-list.dart';
+import 'package:e_commerce/widgets/home_widget/catalog_header.dart';
+import 'package:e_commerce/widgets/themes.dart';
+import 'package:velocity_x/velocity_x.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -22,39 +25,38 @@ class _HomePageState extends State<HomePage> {
     loadData();
   }
 
-
   Future loadData() async {
+   
+
     final catalogJson =
         await rootBundle.loadString("assets/files/catalog.json");
-    final decodedData = jsonDecode(catalogJson);
-    var productsData = decodedData["products"];
-    CatalogModel.items =  List.from(productsData)
-        .map<Item>((item) => Item.fromJson(item))
-        .toList();
-    setState(() {});
+    Map<String, dynamic> jsonData = jsonDecode(catalogJson);
+
+    var productsData = jsonData["products"];
+    CatalogModel.items =
+        List.from(productsData).map((e) => Item.fromJson(e)).toList();
   
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: MyDrawer(),
-      appBar: AppBar(
-        title: Text("Catalog App"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child:  (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
-            ? ListView.builder(
-                itemCount: CatalogModel.items!.length,
-                itemBuilder: (context, index) => ItemWidget(
-                  item: CatalogModel.items![index],
-                ),
-              )
-            : Center(
-                child: CircularProgressIndicator(),
-              )
-      ),
-    );
+        backgroundColor: MyTheme.creamColor,
+        body: SafeArea(
+          child: Container(
+            padding: Vx.m32,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CatalogHeader(),
+                if (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
+                  CatalogList().py16().expand()
+                else
+                  CircularProgressIndicator().centered().expand(),
+              ],
+            ),
+          ),
+        ));
   }
 }
